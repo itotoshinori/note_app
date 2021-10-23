41423ff5bfb575b772ad7a5005394f2cf139e448
T-Ito <you@example.com>
2021-10-23 17:19:20 +0900

    datedis-step

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -106,7 +106,7 @@ export default {
       if (window.confirm("NO." + id + "を本当に削除しますか？")) {
         axios.delete("/api/memos/" + id)  
       } 
-      this.setMemo()  
+      window.location.reload(); 
     }
   }
 }
