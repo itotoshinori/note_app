02b31a36254072e3ffc4b8ededf7c8ab7962ae41
T-Ito <you@example.com>
2021-10-28 10:07:46 +0900

    ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'top/index'
-  #devise_for :users   
+  devise_for :users   
   get '/', to: 'home#index'
   namespace :api, format: 'json' do
     resources :memos, only: [:index, :create, :destroy, :update]
