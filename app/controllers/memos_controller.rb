caf8159bf1cf72075f2ab91bce035a60bd03f73e
T-Ito <you@example.com>
2021-10-31 03:45:10 +0900

    twitter絞り込み

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,7 +1,7 @@
 class MemosController < ApplicationController
   def show
     @memos = Memo.where("public = ?", true)
-    @memo = @memo.find(params[:id]) if @memos.present?
+    @memo = @memos.find(params[:id]) if @memos.present?
     render :layout => nil
   end
 end
