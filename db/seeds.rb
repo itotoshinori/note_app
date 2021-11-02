ef8d8c15466e5513ab8943cd06e938d221b74a61
T-Ito <you@example.com>
2021-11-02 22:12:29 +0900

    データ整備

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,33 @@ memos = Memo.where(user_id:nil)
     memo.save
   end
 end
+
+memos = []
+memos = Memo.where(important:nil)
+(memos).each do |memo|
+  memo = Memo.find(memo.id)
+  if memo.present?
+    memo.important = false
+    memo.save
+  end
+end
+
+memos = []
+memos = Memo.where(twitter:nil)
+(memos).each do |memo|
+  memo = Memo.find(memo.id)
+  if memo.present?
+    memo.twitter = false
+    memo.save
+  end
+end
+
+memos = []
+memos = Memo.where(public:nil)
+(memos).each do |memo|
+  memo = Memo.find(memo.id)
+  if memo.present?
+    memo.public = false
+    memo.save
+  end
+end
