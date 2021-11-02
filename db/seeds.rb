b1261250d9b6e6348c7dc740f779f7468d64386a
T-Ito <you@example.com>
2021-11-02 21:50:31 +0900

    データ整備

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
-5.times do
-    Memo.create(
-      title: Faker::Lorem.word,
-      description: Faker::Lorem.sentence,
-    )
+memo = []
+memos = Memo.where(user_id:nil)
+(memos).each do |memo|
+  memo = Memo.find(memo.id)
+  if memo.present?
+    memo.user_id = 1
+    memo.save
+  end
 end
